adbeb175fb5f77befa58052351766a39db6322ea
Romein van Buren <romein@vburen.nl>
2022-02-17 16:14:52 +0100

    Update SvelteKit
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,6 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-
-	kit: {
-		target: '#svelte'
-	}
 };
 
 export default config;
